dd5111a5c5009dd60c7d05e6047a444d54c239eb
Dzzt <parifuransu@gmail.com>
2026-04-07 09:33:53 +0900

    set hop can search for all directions

--- pluginconfig/config_hop.lua
+++ pluginconfig/config_hop.lua
@@ -1,6 +1,6 @@
 -- configuration for hadronized/hop.nvim
 
-local keymap = vim.keymap
+-- local keymap = vim.keymap
 
 
 local hop = require('hop')
@@ -9,19 +9,18 @@ hop.setup({})
 local directions = require('hop.hint').HintDirection
 
 keymap.set('n', '<Leader>f', function()
-  hop.hint_char1({ direction = directions.AFTER_CURSOR })
-end, {noremap = true})
+  hop.hint_char1({})
+end, { noremap = true })
 
 -- hint-offset = -1 で t
 keymap.set('n', '<Leader>t', function()
-  hop.hint_char1({ direction = directions.AFTER_CURSOR, hint_offset = -1 })
-end, {noremap = true})
+  hop.hint_char1({ hint_offset = -1 })
+end, { noremap = true })
 
 keymap.set('n', '<Leader><Leader>f', function()
-  hop.hint_char2({ direction = directions.AFTER_CURSOR })
-end, {noremap = true})
+  hop.hint_char2({})
+end, { noremap = true })
 
 keymap.set('n', '<Leader><Leader>t', function()
-  hop.hint_char2({ direction = directions.AFTER_CURSOR, hint_offset = -1 })
-end, {noremap = true})
-
+  hop.hint_char2({ hint_offset = -1 })
+end, { noremap = true })
