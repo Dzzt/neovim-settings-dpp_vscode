f8c108400cad7966d8d65415353c5cccf3005cf8
Dzzt <parifuransu@gmail.com>
2026-04-07 09:18:56 +0900

    start using vscode api on neovim

--- init.lua
+++ init.lua
@@ -8,29 +8,29 @@ local api = vim.api
 --  Basic settings
 
 --using different Shada file for vscode neovim
-opt.shadafile = 'NONE'
+opt.shadafile = "NONE"
 
-local vscnvim_dir = fn.expand('~/.config/nvim_vscode')
+local vscnvim_dir = fn.expand("~/.config/nvim_vscode")
 
 opt.runtimepath:append(vscnvim_dir)
-opt.runtimepath:append(vscnvim_dir .. '/lua')
-
+opt.runtimepath:append(vscnvim_dir .. "/lua")
 
 -- Load configs
 local configs = {
-    "vsc_options_base_before",
-    "vsc_plugins",
-    "vsc_options_base_after",
-    "vsc_options_editting",
-    "vsc_keymaps",
+	"vsc_options_base_before",
+	"vsc_plugins",
+	"vsc_options_base_after",
+	"vsc_options_editting",
+	"vsc_keymaps",
+	"vsc_vscodeapi",
 }
 
 for _, config in ipairs(configs) do
-    require(config)
+	require(config)
 end
 
 --manage IME
-api.nvim_create_autocmd({ 'InsertLeave', 'FocusGained' }, {
-    pattern = { "*" },
-    command = "silent! !" .. fn.expand("~/bin/zenhan.exe") .. " 0",
+api.nvim_create_autocmd({ "InsertLeave", "FocusGained" }, {
+	pattern = { "*" },
+	command = "silent! !" .. fn.expand("~/bin/zenhan.exe") .. " 0",
 })
