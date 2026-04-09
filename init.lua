bbe7787c61ef4f7c3832d1d3c8a6d70f928fe3cd
Dzzt <parifuransu@gmail.com>
2026-04-09 12:48:19 +0900

    to fix glitches from neovimapi.lua

--- init.lua
+++ init.lua
@@ -1,13 +1,9 @@
 --  Begin init.vim for Visual Studio Code
--- -- -- --
-
-
---
+-- -- --------------------------------------------------
 local fn = vim.fn
 local opt = vim.opt
 local api = vim.api
 
---  --------------------------------------------------
 --  Basic settings
 
 --using different Shada file for vscode neovim
@@ -25,7 +21,6 @@ local configs = {
 	"vsc_options_base_after",
 	"vsc_options_editting",
 	"vsc_keymaps",
-	"vsc_vscodeapi",
 }
 
 for _, config in ipairs(configs) do
